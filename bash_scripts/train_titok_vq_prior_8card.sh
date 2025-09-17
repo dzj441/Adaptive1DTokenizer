d75bab2362a5d3808d22c3484ce3224c4ad51541
kooWZ <lykong22@m.fudan.edu.cn>
2025-09-17 23:36:53 +0800

    quickfix: fix script for prior loss to multicard

--- bash_scripts/train_titok_vq_prior_8card.sh
+++ bash_scripts/train_titok_vq_prior_8card.sh
@@ -1,25 +1,13 @@
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7\
-# WANDB_API_KEY=0022f2e7631e0264b23083ff83e6d0ca32ebb89e\
-# WANDB_MODE=online\
-# WANDB_PROJECT=titok_bl128_vq\
-# WANDB_NAME=titok_bl128_vq_8card_Prior\ 
-# accelerate launch \
-#   --num_machines=1 \
-#   --num_processes=8 \
-#   --machine_rank=0 scripts/train_titok.py config=configs/training/adaptive1DTokenzier/titok_bl128_vq_prior.yaml \
-#     experiment.project="titok_bl128_vq" \
-#     experiment.name="titok_bl128_vq_8card_Prior" \
-#     experiment.output_dir="titok_bl128_vq_8card_Prior" \
-
-CUDA_VISIBLE_DEVICES=0\
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7\
 WANDB_API_KEY=0022f2e7631e0264b23083ff83e6d0ca32ebb89e\
 WANDB_MODE=online\
 WANDB_PROJECT=titok_bl128_vq\
 WANDB_NAME=titok_bl128_vq_8card_Prior\ 
 accelerate launch \
   --num_machines=1 \
-  --num_processes=1 \
+  --num_processes=8 \
   --machine_rank=0 scripts/train_titok.py config=configs/training/adaptive1DTokenzier/titok_bl128_vq_prior.yaml \
     experiment.project="titok_bl128_vq" \
     experiment.name="titok_bl128_vq_8card_Prior" \
     experiment.output_dir="titok_bl128_vq_8card_Prior" \
+
